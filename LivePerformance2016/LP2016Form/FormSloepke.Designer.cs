743389b9b0be51958a3e025e7b274d22445bef0f
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-22 16:54:50 +0200

    UI mockup

---
+++ LivePerformance2016/LP2016Form/FormSloepke.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LP2016Form
+{
+    partial class FormSloepke
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tpMain = new System.Windows.Forms.TabPage();
+            this.tpOverview = new System.Windows.Forms.TabPage();
+            this.tcMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcMain
+            // 
+            this.tcMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tcMain.Controls.Add(this.tpMain);
+            this.tcMain.Controls.Add(this.tpOverview);
+            this.tcMain.Location = new System.Drawing.Point(0, 1);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(1068, 685);
+            this.tcMain.TabIndex = 0;
+            // 
+            // tpMain
+            // 
+            this.tpMain.Location = new System.Drawing.Point(4, 22);
+            this.tpMain.Name = "tpMain";
+            this.tpMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tpMain.Size = new System.Drawing.Size(1060, 659);
+            this.tpMain.TabIndex = 0;
+            this.tpMain.Text = "Hoofdpagina";
+            this.tpMain.UseVisualStyleBackColor = true;
+            // 
+            // tpOverview
+            // 
+            this.tpOverview.Location = new System.Drawing.Point(4, 22);
+            this.tpOverview.Name = "tpOverview";
+            this.tpOverview.Size = new System.Drawing.Size(1060, 659);
+            this.tpOverview.TabIndex = 2;
+            this.tpOverview.Text = "Overzicht";
+            this.tpOverview.UseVisualStyleBackColor = true;
+            // 
+            // FormSloepke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 682);
+            this.Controls.Add(this.tcMain);
+            this.MaximumSize = new System.Drawing.Size(1080, 720);
+            this.MinimumSize = new System.Drawing.Size(1080, 720);
+            this.Name = "FormSloepke";
+            this.Text = "Sloepke";
+            this.tcMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tpMain;
+        private System.Windows.Forms.TabPage tpOverview;
+    }
+}
+
