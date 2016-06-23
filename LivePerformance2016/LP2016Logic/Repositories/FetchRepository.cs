6a499654ce91e42379b435ca768656b3897aa08d
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-06-23 10:42:16 +0200

    Added logic layer.

---
+++ LivePerformance2016/LP2016Logic/Repositories/FetchRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using LP2016Database.Context;
+using LP2016Database.Models;
+using LP2016Lib.Classes;
+
+namespace LP2016Logic.Repositories
+{
+    public class FetchRepository
+    {
+        private static FetchRepository _instance;
+        private readonly IFetchContext _context;
+
+        public static FetchRepository Instance => _instance ?? (_instance = new FetchRepository());
+
+        private FetchRepository()
+        {
+            _context = new FetchSqlContext();
+        }
+
+        public List<Renter> GetAllRenters()
+        {
+            try
+            {
+                return _context.GetAllRenters();
+            }
+            catch (Exception)
+            {              
+                throw;
+            }
+        }
+
+        public List<Contract> GetAllContracts()
+        {
+            try
+            {
+                return _context.GetAllContracts();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public List<Article> GetAllArticles()
+        {
+            try
+            {
+                return _context.GetAllArticles();
+            }
+            catch (Exception)
+            {              
+                throw;
+            }
+        }
+
+        public List<Boat> GetAllBoats()
+        {
+            try
+            {
+                return _context.GetAllBoats();
+            }
+            catch (Exception)
+            {               
+                throw;
+            }
+        }
+
+        public List<Water> GetAllWaters()
+        {
+            try
+            {
+                return _context.GetAllWaters();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        } 
+    }
+}
---
+++ LivePerformance2016/LP2016Logic/Repositories/SaveRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LP2016Database.Context;
+using LP2016Database.Models;
+using LP2016Lib.Classes;
+using LP2016Lib.Interfaces;
+
+namespace LP2016Logic.Repositories
+{
+    public class SaveRepository
+    {
+        private static SaveRepository _instance;
+        private ISaveContext _context;
+
+        public static SaveRepository Instance => _instance ?? (_instance = new SaveRepository());
+
+        private SaveRepository()
+        {
+            _context = new SaveSqlContext();
+        }
+
+        public void SaveContract(DateTime startDate, DateTime endDate, Renter renter, List<Boat> boats, List<Article> articles)
+        {
+            try
+            {
+                var contract = new Contract(startDate, endDate, renter);
+                boats.ForEach(x => contract.AddItem(x));
+                articles.ForEach(x => contract.AddItem(x));
+                
+                _context.SaveContract(contract);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public void SaveRenter(string name, string email)
+        {
+            try
+            {
+                var renter = new Renter(name, email);
+                _context.SaveRenter(renter);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+    }
+}
